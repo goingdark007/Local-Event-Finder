9a2218e65b6848523290d68a57e61357347d8bc4
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2026-05-15 14:20:22 +0600

    Added Code for local event finder app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.of26_local_event_finder_app"
+    namespace = "com.nazmul.local_event_finder_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.of26_local_event_finder_app"
+        applicationId = "com.nazmul.local_event_finder_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
